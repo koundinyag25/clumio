e1ae4e23f42fa70a5e3095d3de79eb74daca8c35
koundinya <koundinya@koundinyas-MacBook-Pro.local>
2023-02-05 23:25:07 +0530

    rm unwanted componments

--- src/pages/movieListpage/components/searchInput/searchInput.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const SearchInput = ({ onInputChange, palceholder }) => {
-  return;
-  <div className={sx.inputContainer}>
-    <div className={sx.searchIcon}></div>
-    <input className={sx.input} palceholder={palceholder} />;
-  </div>;
-};
